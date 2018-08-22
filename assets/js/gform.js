0a1e3a6fc8363f449138f01c830215ccb5c6b776
Andrew Bin Haji <leet-shadow@hotmail.com>
2018-08-22 17:16:53 -0400

    Attempt to redirect users after a submit

--- assets/js/gform.js
+++ assets/js/gform.js
@@ -6,3 +6,9 @@ function correctCaptcha() {
         $(this).find(':input[type="submit"]').prop('disabled', false);
     });
 }
+$('form').submit(function(){
+    $.post('www.tenumbra.com/thankyou', function() {
+        window.location = 'http://google.com';
+    });
+    return false;
+});
