0372983801a03c0609d249c27a5ecb9027635b06
Andrew Haji <Ahaji@CastGroup.Local>
2018-08-22 16:23:16 -0400

    Added reCaptcha v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 (function($) {
 
+	document.getElementById("submitButton").disabled = true;
+
+	function enableBtn(){
+		document.getElementById("submitButton").disabled = false;
+	}
+
 	skel.breakpoints({
 		xlarge:	'(max-width: 1680px)',
 		large:	'(max-width: 1280px)',
