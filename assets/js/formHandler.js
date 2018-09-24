a6efb0e55d4724ba049e0e76f227c66506848a59
Tenumbra <leet-shadow@hotmail.com>
2018-09-24 09:42:41 -0400

    Reverted half of the last chances due to faulty response

--- assets/js/formHandler.js
+++ assets/js/formHandler.js
@@ -52,7 +52,7 @@
   
       // add form-specific values into the data
       formData.formDataNameOrder = JSON.stringify(fields);
-      //formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
+      formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
       formData.formGoogleSendEmail = form.dataset.email || ""; // no email by default
       unset($_POST['g-recaptcha-response']);
   
