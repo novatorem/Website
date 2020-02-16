b2a335e8557ff617bed8e005c34bfae64def4cec
Andrew Novac <leet-shadow@hotmail.com>
2020-02-16 23:03:00 +0900

    Depend on (youtubeID.len == 11)

--- x/Melodify/replacer.js
+++ x/Melodify/replacer.js
@@ -1,9 +1,9 @@
 let urlParams = new URLSearchParams(window.location.search)
 
 let urlParamString = urlParams.toString()
-let youtube = "https://www.youtube.com/embed/".concat(urlParamString.substring(0, urlParamString.length - 2))
+let youtube = "https://www.youtube.com/embed/".concat(urlParamString.substring(0, 11))
 youtube = youtube.concat("?autoplay=1&controls=0&disablekb=1&fs=0&modestbranding=1&start=")
-youtube = youtube.concat(urlParamString.slice(-2,-1))
+youtube = youtube.concat(urlParamString.substring(11))
 
 let element = document.createElement("iframe")
 element.src=youtube
