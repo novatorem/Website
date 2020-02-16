cfc9a36727d8e92de8224ee50e0c1f351c07f7d7
Andrew Novac <leet-shadow@hotmail.com>
2020-02-16 23:09:00 +0900

    Switching to use of key/values

--- x/Melodify/replacer.js
+++ x/Melodify/replacer.js
@@ -1,9 +1,9 @@
 let urlParams = new URLSearchParams(window.location.search)
 
 let urlParamString = urlParams.toString()
-let youtube = "https://www.youtube.com/embed/".concat(urlParamString.substring(0, 11))
+let youtube = "https://www.youtube.com/embed/".concat(urlParams.get("videoCode"))
 youtube = youtube.concat("?autoplay=1&controls=0&disablekb=1&fs=0&modestbranding=1&start=")
-youtube = youtube.concat(urlParamString.substring(11, -1))
+youtube = youtube.concat(urlParams.get("startTime"))
 
 let element = document.createElement("iframe")
 element.src=youtube
