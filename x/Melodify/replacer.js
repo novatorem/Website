13c304520b5bd21116d0f4c06535472106898051
Andrew Novac <leet-shadow@hotmail.com>
2020-02-16 23:18:00 +0900

    Trying out controls to see issue

--- x/Melodify/replacer.js
+++ x/Melodify/replacer.js
@@ -2,7 +2,7 @@ let urlParams = new URLSearchParams(window.location.search)
 
 let urlParamString = urlParams.toString()
 let youtube = "https://www.youtube.com/embed/".concat(urlParams.get("videoCode"))
-youtube = youtube.concat("?autoplay=1&controls=0&disablekb=1&fs=0&modestbranding=1&start=")
+youtube = youtube.concat("?autoplay=1&disablekb=1&fs=0&modestbranding=1&start=")
 youtube = youtube.concat(urlParams.get("startTime"))
 alert(youtube)
 
